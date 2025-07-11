056df2956b621d84b23208577f1e37801c1d1779
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-07-12 03:18:39 +0700

    fix: update JwtPayload role type to use AccountRole enum for better type safety

--- apps/api/src/auth/interfaces/jwt-payload.interface.ts
+++ apps/api/src/auth/interfaces/jwt-payload.interface.ts
@@ -1,7 +1,9 @@
+import { AccountRole } from 'src/common/enums/account-role.enum';
+
 export interface JwtPayload {
   sub: number; // User ID
   email: string; // User email
-  role: string; // Role của user (bắt buộc, nếu chỉ 1 role)
+  role: AccountRole; // Role của user (bắt buộc, nếu chỉ 1 role)
   name?: string; // (Optional) Tên user nếu bạn muốn truyền
   iat?: number; // (Tự động) Thời gian phát hành token (Issued At)
   exp?: number; // (Tự động) Thời gian hết hạn token (Expiration)
