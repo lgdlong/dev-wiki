4e427b689eda2682ca2411fa265a85ad6a061334
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-07-12 16:59:27 +0700

    feat: enable CORS for frontend access

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,21 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || DEFAULT_PORT; // fallback nếu không có PORT
 
+  // ---- Bật CORS cho FE truy cập ----
+  app.enableCors({
+    // FE Next.js localhost
+    origin: 'http://localhost:3000',
+
+    // Cho phép nếu dùng cookie/session/token
+    // Cho phép credentials: "include" bên FE
+    credentials: true,
+    // Cho phép các phương thức HTTP
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+
+    // Cho phép các header cần thiết
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   await app.listen(port);
 }
 bootstrap();
