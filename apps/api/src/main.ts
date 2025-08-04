5c394e7cf9926c3476977d175147c049f0aaf8a5
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-08-04 21:25:55 +0700

    feat: add Swagger documentation for api endpoints

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,10 +3,23 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { DEFAULT_FRONTEND_URL, DEFAULT_PORT } from './common/constants';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Cấu hình Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth() // Nếu dùng JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   // Lấy ConfigService từ app context
   const configService = app.get(ConfigService);
@@ -22,7 +35,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   });
-
   await app.listen(port);
 }
 bootstrap();
---
+++ apps/web/src/app/docs/page.tsx
@@ -0,0 +1,14 @@
+import { styleText } from "util";
+
+export default function DocsPage() {
+  const BACKEND_LOCAL_HOST = "http://localhost:8000";
+  const API_DOCS_URL = `${BACKEND_LOCAL_HOST}/api/docs`;
+  return (
+    <div>
+      <h1>Dev Wiki Document</h1>
+      <a className="text-blue-500" href={API_DOCS_URL}>
+        API Docs
+      </a>
+    </div>
+  );
+}
