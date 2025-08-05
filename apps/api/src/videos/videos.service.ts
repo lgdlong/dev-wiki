ec4778700c1f4b63b2e00d4b1427814d6a46ac03
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-08-05 20:37:34 +0700

    feat: update video creation api to fetch YouTube metadata and handle errors

--- apps/api/src/videos/videos.service.ts
+++ apps/api/src/videos/videos.service.ts
@@ -1,9 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { google } from 'googleapis';
 import { Video } from './entities/video.entity';
-import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
+import { RequestCreateVideo } from './interfaces/request-create-video.interface';
+import { YoutubeMetadata } from './interfaces/youtube-metadata.interface';
+import { toVideoEntity } from 'src/mappers/video.mapper';
 
 @Injectable()
 export class VideosService {
@@ -12,8 +15,27 @@ export class VideosService {
     private videoRepository: Repository<Video>,
   ) {}
 
-  async create(createVideoDto: CreateVideoDto): Promise<Video> {
-    const video = this.videoRepository.create(createVideoDto);
+  private YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || '';
+
+  // Tạo video, tự động lấy metadata từ Youtube
+  async create(requestCreateVideo: RequestCreateVideo): Promise<Video> {
+    // Lấy metadata từ Youtube
+    const metadata: YoutubeMetadata | undefined = await this.getYoutubeMetadata(
+      requestCreateVideo.youtubeId,
+    );
+    if (!metadata) {
+      throw new HttpException('Cannot fetch video metadata', 400);
+    }
+
+    // Mapping qua hàm mapper
+    const videoEntity: Partial<Video> = toVideoEntity(
+      requestCreateVideo,
+      metadata,
+    );
+
+    // Khởi tạo entity bằng repository
+    const video: Video = this.videoRepository.create(videoEntity);
+
     return await this.videoRepository.save(video);
   }
 
@@ -62,5 +84,39 @@ export class VideosService {
     }
 
     return video;
+  }
+
+  private async getYoutubeMetadata(
+    youtubeId: string,
+  ): Promise<YoutubeMetadata> {
+    try {
+      const youtube = google.youtube({
+        version: 'v3',
+        auth: this.YOUTUBE_API_KEY,
+      });
+
+      const res = await youtube.videos.list({
+        id: [youtubeId],
+        part: ['snippet'],
+      });
+
+      const item = res.data.items?.[0];
+      if (!item) return {};
+
+      return {
+        title: item.snippet?.title ?? undefined,
+        // full descriptions, its can be long
+        description: item.snippet?.description ?? undefined,
+        // thumbnail picture url, size medium
+        thumbnail: item.snippet?.thumbnails?.medium?.url ?? '',
+        // the channel uploaded this video on YouTube
+        channelTitle: item.snippet?.channelTitle ?? undefined,
+        // the number of this video's views
+        viewCount: item.statistics?.viewCount ?? undefined,
+      };
+    } catch (error) {
+      console.error('Error fetching YouTube metadata:', error);
+      throw new HttpException('Failed to fetch YouTube metadata', 500);
+    }
   }
 }
