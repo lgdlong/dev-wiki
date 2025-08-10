7f0dd048ad22d074758e93afe4e6f370aa1da636
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-08-11 06:31:16 +0700

    fix: update express module declaration for Request interface

--- apps/api/src/types/express/index.d.ts
+++ apps/api/src/types/express/index.d.ts
@@ -1,7 +1,7 @@
 // apps/api/src/types/express/index.d.ts
 import { GoogleProfile } from '../../auth/interfaces/google-profile.interface';
 
-declare module 'express-serve-static-core' {
+module 'express-serve-static-core' {
   interface Request {
     user?: GoogleProfile; // hoặc User, hoặc union nhiều loại
   }
