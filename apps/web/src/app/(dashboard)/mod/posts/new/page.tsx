838dc9c208c13593b56e9ff3f490f82a22edd3f8
F15 <duongquocthai368@gmail.com>
2025-08-19 23:53:12 +0700

    "feat: add page post composer #35" "feat: add toast ui markdown"
    This reverts commit 713497409f7a8f8e10967121928ca9e96d2319d5.

---
+++ apps/web/src/app/(dashboard)/mod/posts/new/page.tsx
@@ -0,0 +1,12 @@
+import PostComposer from '@/components/composer/post-composer';
+
+export const metadata = { title: 'New Post' };
+
+export default function NewModPostPage() {
+  return (
+    <main className="mx-auto w-full max-w-10xl p-4 sm:p-6 md:p-8">
+        <h1 className='flex items-center justify-center text-4xl mb-5'>Create Post</h1>
+      <PostComposer />
+    </main>
+  );
+}
