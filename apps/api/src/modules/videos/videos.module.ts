d5e796241fee00e0858692711b19c4bce6e4fe88
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-11-30 02:50:59 +0700

    feat: thêm api lấy videos bởi tag id và tag name

--- apps/api/src/modules/videos/videos.module.ts
+++ apps/api/src/modules/videos/videos.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideosService } from './videos.service';
 import { VideosController } from './videos.controller';
 import { Video } from './entities/video.entity';
+import { VideoTagsModule } from '../video-tags/video-tags.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Video])],
+  imports: [TypeOrmModule.forFeature([Video]), VideoTagsModule],
   controllers: [VideosController],
   providers: [VideosService],
   exports: [VideosService],
